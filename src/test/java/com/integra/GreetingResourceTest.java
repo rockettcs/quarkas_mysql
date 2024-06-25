fcc3b07a0f8c9b9f57824eb38d3e2952e5584161
rockettcs <89966003+rockettcs@users.noreply.github.com>
2024-06-25 20:28:53 +0530

    Update GreetingResourceTest.java

--- src/test/java/com/integra/GreetingResourceTest.java
+++ src/test/java/com/integra/GreetingResourceTest.java
@@ -11,10 +11,9 @@ class GreetingResourceTest {
     @Test
     void testHelloEndpoint() {
         given()
-          .when().get("/hello")
+          .when().get("/cache/get?key=key")
           .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
+             .statusCode(200);
     }
 
 }
